b867a97204b8e796f5ce4f40a3c755a54dda337e
Kenel1 <gdenzel.vianna@gmail.com>
2021-08-06 01:11:09 -0300

    +

--- web-client/lab01/q5/script.js
+++ web-client/lab01/q5/script.js
@@ -1,12 +1,12 @@
-function numPrime(i) {
-    for (var div = 2; div < i; div++) 
+function numPrime (i) {
+    for (let div = 2; div < i; div++) 
         if (i % div == 0) { 
             return false;
         }
         return true;
 }
 
-for (var i = 2; i < 1000; i++) 
-    if (numPrime(i)) {
-        console.log(i)
+for (let i = 2; i < 1000; i++) 
+    if (numPrime (i)) {
+        console.log (i)
     }
