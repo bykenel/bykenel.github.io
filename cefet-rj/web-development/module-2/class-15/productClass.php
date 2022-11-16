af0594c8249f85c00fbe0706ca231ad546c06c4a
Kenel1 <gdenzel.vianna@gmail.com>
2022-11-16 19:36:48 -0300

    +

---
+++ cefet-rj/web-development/module-2/class-15/productClass.php
@@ -0,0 +1,106 @@
+<?php
+    class Produto {
+        //Atributos
+            private int $codigo = 0;
+            private string $descricao = "";
+            private float $precoDeCusto = 0;
+            private int $estoque = 0;
+            private string $fornecedor = "";
+            private static int $numDeProdutos = 0;
+        
+        //Método construtor
+            public function __construct(int $codigo, string $descricao, float $precoDeCusto, string $fornecedor, int $estoque = 0) {
+                $this -> atribuirCodigo($codigo);
+                $this -> atribuirDescricao($descricao);
+                $this -> atribuirPrecoDeCusto($precoDeCusto);
+                $this -> atribuirEstoque($estoque);
+                $this -> atribuirFornecedor($fornecedor);
+                Produto::$numDeProdutos++;
+            }
+            
+        //Métodos acessores 
+            //codigo
+            public function recuperarCodigo():int {
+                return $this -> codigo;
+            }
+
+            public function atribuirCodigo(int $codigo) {
+                if (strlen($codigo < 4)) {
+                    echo "Valor inválido: é necessário ao menos 4 caracteres.";
+                    return;
+                }
+                $this -> codigo = $codigo;
+            }
+
+            //descricao
+            public function recuperarDescricao():string {
+                return $this -> descricao;
+            }
+
+            public function atribuirDescricao(string $descricao) {
+                if (strlen($descricao < 6)) {
+                    echo "Valor inválido: é necessário ao menos 6 caracteres.";
+                    return;
+                }
+                $this -> descricao = $descricao;
+            }
+
+            //precoDeCusto
+            public function recuperarPrecoDeCusto():float {
+                return $this -> precoDeCusto;
+            }
+
+            public function atribuirPrecoDeCusto(float $precoDeCusto) {
+                if (!($precoDeCusto > 0)) {
+                    echo "Valor inválido: é necessário que seja maior do que 0.";
+                    return;
+                }
+                $this -> precoDeCusto = $precoDeCusto;
+            }
+
+            //estoque
+            public function recuperarEstoque():int {
+                return $this -> estoque;
+            }
+
+            public function atribuirEstoque(int $estoque) {
+                if (!($estoque >= 0)) {
+                    echo "Valor inválido: é necessário que seja positivo.";
+                    return;
+                }
+                $this -> estoque = $estoque;
+            }
+
+            //fornecedor
+            public function recuperarFornecedor():string {
+                return $this -> fornecedor;
+            }
+
+            public function atribuirFornecedor(string $fornecedor) {
+                if (strlen($fornecedor < 6)) {
+                    echo "Valor inválido: é necessário ao menos 6 caracteres.";
+                    return;
+                }
+                $this -> fornecedor = $fornecedor;
+            }
+        
+        //Funções de validação/úteis
+            public static function obterNumDeProdutos():int {
+                return Produto::$numDeProdutos;
+            }
+            
+            public function obterPrecoDeVenda(float $margemDeLucro):void {
+                if ($this -> validaMargemDeLucro($margemDeLucro)) {
+                    $this -> precoDeCusto * $margemDeLucro;
+                }
+            }
+
+            private function validaMargemDeLucro(float $taxa):bool {
+                if (!($taxa > 10 || $taxa < 50)) {
+                    echo "A margem de lucro precisa estar entre 10% e 50%";
+                    exit();
+                }
+                return true;
+            }
+    }
+?>
