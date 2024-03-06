ec716d641fd0df6e1c50f6773c157de0670b4758
Denzel Farias <denzel.vianna@hotmail.com>
2024-03-06 11:52:37 -0300

    index update (namer-app)

--- namer-app/flutter_service_worker.js
+++ namer-app/flutter_service_worker.js
@@ -26,8 +26,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "957b0faa599259bebca92c1825f45310",
-"/": "957b0faa599259bebca92c1825f45310",
+"index.html": "ddcfb96aa456eae16fdf9d47996720ca",
+"/": "ddcfb96aa456eae16fdf9d47996720ca",
 "main.dart.js": "a98141792e095200bfe4e3d51027cc8a",
 "manifest.json": "08bf9a8512ce11aff052b99e3ac32c1f",
 "version.json": "f3750ba2ab524cc05fad3b96b4b45d14"};
